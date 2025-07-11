479825cba81cd30992e048a42b734f84bbdb84de
pedro santilli <pedrosantilli@MacBook-Air-de-pedro-2.local>
2025-07-10 22:06:36 -0300

    comecando projeto

---
+++ idle_legendsTests/idle_legendsTests.swift
@@ -0,0 +1,17 @@
+//
+//  idle_legendsTests.swift
+//  idle_legendsTests
+//
+//  Created by pedro santilli on 10/07/25.
+//
+
+import Testing
+@testable import idle_legends
+
+struct idle_legendsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
